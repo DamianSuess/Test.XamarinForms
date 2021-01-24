f454ff0068a201cf2bee60d43a4fd41d79520f27
Damian Suess <djsuess@gmail.com>
2021-01-23 22:00:42 -0500

    cleanup

--- source/Test.XamForms/ViewModels/CollectionSwipeViewModel.cs
+++ source/Test.XamForms/ViewModels/CollectionSwipeViewModel.cs
@@ -4,11 +4,12 @@
  *  - The RaisePropertyChanged will handle Adding and Removing items well, but not updating.
  *  - In order to have the item's text updated, your object (Customer) can inherit
  *    from the both your Interface (ICustomer) and INotifyableProperty as well.
+ *    Pro Tip:
+ *      The use of the 'ICustomer' interface inside of ObservableCollection is no
+ *      mistake. It's good practice to utilize your base class.
  */
 
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ComponentModel;
 using Prism.Commands;
 using Prism.Navigation;
 using Test.XamForms.Client.Models;
@@ -18,21 +19,19 @@ namespace Test.XamForms.Client.ViewModels
 {
   public class CollectionSwipeViewModel : ViewModelBase
   {
-    private ObservableCollection<CustomerNotifyable> _customers;
+    private ObservableCollection<ICustomer> _customers;
 
     public CollectionSwipeViewModel(INavigationService navigationService, ILogService logService)
       : base(navigationService)
     {
       _log = logService;
-      Customers = new ObservableCollection<CustomerNotifyable>();
-      //// Customers.CollectionChanged += OnCustomersCollectionChanged;
-
+      Customers = new ObservableCollection<ICustomer>();
       InitSample();
     }
 
     private ILogService _log;
 
-    public ObservableCollection<CustomerNotifyable> Customers
+    public ObservableCollection<ICustomer> Customers
     {
       get => _customers;
       set => SetProperty(ref _customers, value);
@@ -40,12 +39,11 @@ namespace Test.XamForms.Client.ViewModels
 
     public DelegateCommand CmdItemAdd => new DelegateCommand(OnItemAdd);
 
-    public DelegateCommand<CustomerNotifyable> CmdItemEdit => new DelegateCommand<CustomerNotifyable>(OnItemEdit);
+    public DelegateCommand<ICustomer> CmdItemEdit => new DelegateCommand<ICustomer>(OnItemEdit);
 
-    public DelegateCommand<CustomerNotifyable> CmdItemRemove => new DelegateCommand<CustomerNotifyable>((c) =>
+    public DelegateCommand<ICustomer> CmdItemRemove => new DelegateCommand<ICustomer>((c) =>
     {
       _log.Debug($"Selected remove item, '{c.Name}'");
-
       Customers.Remove(c);
     });
 
@@ -58,7 +56,7 @@ namespace Test.XamForms.Client.ViewModels
       RaisePropertyChanged(nameof(Customers));
     }
 
-    private void OnItemEdit(CustomerNotifyable customer)
+    private void OnItemEdit(ICustomer customer)
     {
       string name = customer.Name;
       _log.Debug("Selected item to edit, Name=" + name);
@@ -75,33 +73,6 @@ namespace Test.XamForms.Client.ViewModels
       Customers.Add(new CustomerNotifyable("1", "Betty Boop"));
       Customers.Add(new CustomerNotifyable("2", "Sandy Smith"));
       Customers.Add(new CustomerNotifyable("3", "Roxie Road"));
-    }
-
-    private void OnCustomersCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
-    {
-      if (e.Action == NotifyCollectionChangedAction.Add)
-      {
-        _log.Debug("Item Added");
-        ////foreach (Customer item in e.NewItems)
-        ////  item.PropertyChanged += this.OnItemPropertyChanged;
-      }
-      else if (e.Action == NotifyCollectionChangedAction.Remove)
-      {
-        _log.Debug("Item Removed");
-
-        ////foreach (Customer item in e.NewItems)
-        ////  item.PropertyChanged -= this.OnItemPropertyChanged;
-      }
-      else if (e.Action == NotifyCollectionChangedAction.Replace)
-      {
-        _log.Debug("Item Replaced");
-      }
-    }
-
-    private void OnItemPropertyChanged(object sender, PropertyChangedEventArgs e)
-    {
-      ////this.PhotosIHate.Refresh();
-      ////this.PhotosILike.Refresh();
     }
   }
 }
